262472c8f0d36ecfbe672bcbfd229b9276d4ad25
Andrei Ozdon <aahzzyy@gmail.com>
2022-08-28 22:23:29 +0300

    Replaced output configuration for production build.

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,18 @@ const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const common = require("./webpack.common.js");
+const path = require('path');
 
 module.exports = merge(common, {
   mode: "production",
+  output: {
+    filename: "[contenthash].js",
+    charset: true,
+  },
   devtool: "source-map",
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [new MiniCssExtractPlugin({
+    filename: '[contenthash].css'
+  })],
   module: {
     rules: [
       {
